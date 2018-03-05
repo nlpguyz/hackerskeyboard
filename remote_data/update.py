933d7d772d375f019bdc98fc43e6c5515b0480a8
Wunshan Seh <nlpguyz@gmail.com>
2018-03-05 08:30:21 -0800

    Add scripts

--- remote_data/update.py
+++ remote_data/update.py
@@ -2,3 +2,30 @@
 
 # Read each URL and download the IMDF file, and add to the downloadable.json file
 # Set homeUrl to the directory that contains the IMDF file.
+
+import requests
+import json
+
+listFile = "imdf_list.in"
+jsonFile = "downloadable.json"
+
+def appendEntry(jsonOut, url):
+    # read url
+    # change homeUrl
+    # serialize as json
+    print(url)
+    r = requests.get(url)
+    imdf = json.loads(r.content)
+    pos = url.rfind("/")
+    imdf["homeUrl"] = url[:pos+1]
+    jsonList.append(imdf)
+
+jsonList = []
+
+with open(listFile, "r") as file:
+    for line in file:
+        line = line.strip()
+        appendEntry(jsonList, line)
+
+jsonOut = open(jsonFile, "w")
+jsonOut.write(json.dumps(jsonList, indent=4, separators=(',', ': ')))
