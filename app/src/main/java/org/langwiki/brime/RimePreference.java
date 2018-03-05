49a2cc7668e68225270737141036a32caa2bc5ee
Wunshan Seh <nlpguyz@gmail.com>
2018-03-04 16:52:42 -0800

    Add preference

--- app/src/main/java/org/langwiki/brime/RimePreference.java
+++ app/src/main/java/org/langwiki/brime/RimePreference.java
@@ -2,7 +2,9 @@ package org.langwiki.brime;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 
 import java.util.List;
@@ -16,16 +18,19 @@ public class RimePreference extends PreferenceActivity {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.pref_rime);
 
+        // Add schema checkboxes
         List<Map<String, String>> schemas = Rime.getInstance().get_available_schema_list();
 
         PreferenceGroup parent = getPreferenceScreen();
+        PreferenceCategory schemaParent = (PreferenceCategory)findPreference("rime_schemata");
+
         for (Map<String, String> schema : schemas) {
             String name = schema.get("name");
             String id = schema.get("schema_id");
 
             CheckBoxPreference pref = new CheckBoxPreference(this);
             pref.setTitle(name);
-            parent.addPreference(pref);
+            schemaParent.addPreference(pref);
         }
     }
 }
