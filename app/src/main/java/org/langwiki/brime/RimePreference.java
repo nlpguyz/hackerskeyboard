8e162ade2c93b66889724712b0d06aaf7911555a
NlpGuyz <nlpguyz@gmail.com>
2018-11-16 15:49:38 -0800

    Update Preference to API 23

--- app/src/main/java/org/langwiki/brime/RimePreference.java
+++ app/src/main/java/org/langwiki/brime/RimePreference.java
@@ -1,11 +1,13 @@
 package org.langwiki.brime;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
-import android.preference.PreferenceGroup;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 import org.langwiki.brime.schema.SchemaManager;
@@ -14,103 +16,115 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class RimePreference extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+public class RimePreference extends PreferenceActivity {
     private static final String TAG = IMEConfig.TAG + "-Pref";
-
-    private List<CheckBoxPreference> mSchemaPrefs = new ArrayList<>();
-    private SettingManager mSettingsManager;
 
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        mSettingsManager = new SettingManager(this);
-        addPreferencesFromResource(R.xml.pref_rime);
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new RimePreferenceFragment()).commit();
     }
 
-    private void refresh() {
-        // Add schema checkboxes
-        List<Map<String, String>> schemas = Rime.getInstance().get_available_schema_list();
+    public static class RimePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
+        private List<CheckBoxPreference> mSchemaPrefs = new ArrayList<>();
+        private SettingManager mSettingsManager;
 
-        PreferenceGroup parent = getPreferenceScreen();
-        PreferenceCategory schemaParent = (PreferenceCategory)findPreference("rime_schemata");
-
-        schemaParent.removeAll();
-        mSchemaPrefs.clear();
-
-        String selectedId = mSettingsManager.getCurrentRimeSchemaId();
-        CheckBoxPreference first = null;
-        CheckBoxPreference selected = null;
-
-        if (schemas != null) {
-            for (Map<String, String> schema : schemas) {
-                String name = schema.get("name");
-                String id = schema.get("schema_id");
-
-                CheckBoxPreference pref = new CheckBoxPreference(this);
-                pref.setTitle(name);
-                pref.setKey(id);
-
-                if (first == null) {
-                    first = pref;
-                }
-
-                // select previous schema or first
-                if (selected == null && selectedId != null && selectedId.equals(id)) {
-                    selected = pref;
-                }
-
-                pref.setOnPreferenceChangeListener(this);
-                mSchemaPrefs.add(pref);
-                schemaParent.addPreference(pref);
-                pref.setChecked(false);
-            }
+        public RimePreferenceFragment() {
         }
 
-        // Selected is gone. Use first.
-        if (selected == null && first != null) {
-            selected = first;
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mSettingsManager = new SettingManager(getContext());
+            addPreferencesFromResource(R.xml.pref_rime);
         }
 
-        // Toggle here to avoid invoking listeners before all prefs are added
-        if (selected != null) {
-            selected.setChecked(true);
-        }
-    }
+        private void refresh() {
+            // Add schema checkboxes
+            List<Map<String, String>> schemas = Rime.getInstance().get_available_schema_list();
 
-    public void onDeployButton(View v) {
-        SchemaManager.getInstance().redeploy(this, false, true);
-    }
+            PreferenceCategory schemaParent = (PreferenceCategory)findPreference("rime_schemata");
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        refresh();
-    }
+            schemaParent.removeAll();
+            mSchemaPrefs.clear();
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        boolean value = ((Boolean)newValue).booleanValue();
+            String selectedId = mSettingsManager.getCurrentRimeSchemaId();
+            CheckBoxPreference first = null;
+            CheckBoxPreference selected = null;
 
-        // Cannot deselect last schema
-        if (value == false && preference.getKey().equals(mSettingsManager.getCurrentRimeSchemaId())) {
-            return false;
-        }
+            if (schemas != null) {
+                for (Map<String, String> schema : schemas) {
+                    String name = schema.get("name");
+                    String id = schema.get("schema_id");
 
-        // Enforce one selection
-        if (value) {
-            for (CheckBoxPreference p : mSchemaPrefs) {
-                if (!p.getKey().equals(preference.getKey())) {
-                    p.setChecked(false);
+                    CheckBoxPreference pref = new CheckBoxPreference(getContext());
+                    pref.setTitle(name);
+                    pref.setKey(id);
+
+                    if (first == null) {
+                        first = pref;
+                    }
+
+                    // select previous schema or first
+                    if (selected == null && selectedId != null && selectedId.equals(id)) {
+                        selected = pref;
+                    }
+
+                    pref.setOnPreferenceChangeListener(this);
+                    mSchemaPrefs.add(pref);
+                    schemaParent.addPreference(pref);
+                    pref.setChecked(false);
                 }
             }
 
-            // Save
-            mSettingsManager.setCurrentRimeSchemaId(preference.getKey());
+            // Selected is gone. Use first.
+            if (selected == null && first != null) {
+                selected = first;
+            }
 
-            // Enable
-            SchemaManager.getInstance().selectSchema(preference.getKey());
+            // Toggle here to avoid invoking listeners before all prefs are added
+            if (selected != null) {
+                selected.setChecked(true);
+            }
         }
 
-        return true;
+        public void onDeployButton(View v) {
+            SchemaManager.getInstance().redeploy(getContext(), false, true);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            refresh();
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            boolean value = ((Boolean)newValue).booleanValue();
+
+            // Cannot deselect last schema
+            if (value == false && preference.getKey().equals(mSettingsManager.getCurrentRimeSchemaId())) {
+                return false;
+            }
+
+            // Enforce one selection
+            if (value) {
+                for (CheckBoxPreference p : mSchemaPrefs) {
+                    if (!p.getKey().equals(preference.getKey())) {
+                        p.setChecked(false);
+                    }
+                }
+
+                // Save
+                mSettingsManager.setCurrentRimeSchemaId(preference.getKey());
+
+                // Enable
+                SchemaManager.getInstance().selectSchema(preference.getKey());
+            }
+
+            return true;
+        }
+
     }
 }
